c8421a411f953836143dbb5841408de378b8cd6c
Arnim <arnime@kpn-cert.nl>
2019-01-22 00:51:09 +0100

    Fixed compatibility issues with raising errors

--- netchecker.py
+++ netchecker.py
@@ -41,12 +41,12 @@ def _is_subnet_of(a, b):
     try:
         # Always false if one is v4 and the other is v6.
         if a._version != b._version:
-            raise TypeError(f"{a} and {b} are not of the same version")
+            raise TypeError("{a} and {b} are not of the same version")
         return (b.network_address <= a.network_address and
                 b.broadcast_address >= a.broadcast_address)
     except AttributeError:
-        raise TypeError(f"Unable to test subnet containment "
-                        f"between {a} and {b}")
+        raise TypeError("Unable to test subnet containment "
+                        "between {a} and {b}")
 
 def subnet_of(a, b):
     """Return True if this network is a subnet of other."""
